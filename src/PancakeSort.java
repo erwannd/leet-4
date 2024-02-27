2fa2780f3095b962ca97ec336ef1bf280cc235c2
Andrew Wihardja <andrewadi96@gmail.com>
2024-02-26 21:55:10 -0800

    add problem description

--- src/PancakeSort.java
+++ src/PancakeSort.java
@@ -1,8 +1,34 @@
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
 
+/**
+ * Given an array of integers arr, sort the array by performing a series of pancake flips.
+ * In one pancake flip we do the following steps:
+ *  1. Choose an integer k where 1 <= k <= arr.length.
+ *  2. Reverse the sub-array arr[0...k-1] (0-indexed).
+ * For example, if arr = [3,2,1,4] and we performed a pancake flip choosing k = 3,
+ * we reverse the sub-array [3,2,1], so arr = [1,2,3,4] after the pancake flip at k = 3.
+ * Return an array of the k-values corresponding to a sequence of pancake flips that sort arr.
+ * Any valid answer that sorts the array within 10 * arr.length flips will be judged as correct.
+
+ * Example 1:
+ *      Input: arr = [3,2,4,1]
+ *      Output: [4,2,4,3]
+ *      Explanation:
+ *      We perform 4 pancake flips, with k values 4, 2, 4, and 3.
+ *      Starting state: arr = [3, 2, 4, 1]
+ *      After 1st flip (k = 4): arr = [1, 4, 2, 3]
+ *      After 2nd flip (k = 2): arr = [4, 1, 2, 3]
+ *      After 3rd flip (k = 4): arr = [3, 2, 1, 4]
+ *      After 4th flip (k = 3): arr = [1, 2, 3, 4], which is sorted.
+
+ * Example 2:
+ *      Input: arr = [1,2,3]
+ *      Output: []
+ *      Explanation: The input is already sorted, so there is no need to flip anything.
+ *      Note that other answers, such as [3, 3], would also be accepted.
+ */
 public class PancakeSort {
     public List<Integer> pancakeSort(int[] arr) {
         List<Integer> output = new LinkedList<>();
@@ -60,14 +86,7 @@ public class PancakeSort {
     }
 
     public static void main(String[] args) {
-//        int[] arr = {1,2,3};
         int[] arr = {3,2,4,1};
-//        int size = 100;
-//        int[] arr = new int[size];
-//        Random random = new Random();
-//        for (int i = 0; i < arr.length; i++) {
-//            arr[i] = random.nextInt(100);
-//        }
 
         System.out.printf("before: %s\n", Arrays.toString(arr));
         PancakeSort pancake = new PancakeSort();
